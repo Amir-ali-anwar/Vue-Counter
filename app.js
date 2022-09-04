69db66a35ee303eff0914688b201a3b6315d0cd1
Amir Ali Anwar <amiralianwar611@gmail.com>
2022-09-04 21:40:00 +0500

    counter complete

---
+++ app.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.counter = this.counter + 1;
+    },
+    reduce() {
+      this.counter = this.counter - 1;
+      // this.counter--;
+    },
+    reset() {
+      this.counter = 0;
+      // this.counter--;
+    },
+  },
+});
+
+app.mount('#events');
